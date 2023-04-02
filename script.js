eaa92b65fcac432b79115b9ffe9dfe6d70692c7a
GustavoTostes <89785678+GustavoTostes@users.noreply.github.com>
2023-04-02 18:05:49 -0300

    first commit

---
+++ script.js
@@ -0,0 +1,17 @@
+function interagirModalSobreMim(acao) {
+
+    $('#modalSobreMim').modal(acao);
+
+}
+
+function toGitHub() {
+
+    window.open('https://github.com/GustavoTostes')
+
+}
+
+function toLinkedin() {
+
+    window.open('https://www.linkedin.com/in/gustavo-tostes-soares-b2141822a/')
+
+}
